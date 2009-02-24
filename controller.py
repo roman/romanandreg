3051a272d38e8fdc230a93846f0a249f95f2f4b1
Roman Gonzalez <romanandreg@gmail.com>
2009-02-24 14:37:50 -0800

    Adding my twitter timeline to the home page

---
+++ controller.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import os, sys
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+# We load the vendors folder...
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'vendor')))
+import twitter
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    view = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(view, {}))
+
+
+class TwitterTimeline(webapp.RequestHandler):
+  def get(self):
+    api = twitter.Api('romanandreg', 'blahblah')
+    api.SetCache(None)
+    timeline = api.GetUserTimeline('romanandreg')[0:10]
+    view = os.path.join(os.path.dirname(__file__), 'twitter.html')
+    keys = {
+      'timeline': timeline,
+    }
+    self.response.out.write(template.render(view, keys))
+  
+application = webapp.WSGIApplication(
+                                    [('/', MainPage),
+                                     ('/twitter', TwitterTimeline)])
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
